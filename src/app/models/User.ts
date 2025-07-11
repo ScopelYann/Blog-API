3a71a3e21f7f6aa51c189a259210922aba5667e2
Scopel_Yan <160073076+ScopelYann@users.noreply.github.com>
2025-07-10 21:44:52 -0300

    init03

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes, Model } from "sequelize";
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 interface UserAtributes {
   id: string;
@@ -13,14 +13,14 @@ interface UserAtributes {
 }
 
 class User extends Model<UserAtributes> {
-  public id!: string;
-  public name!: string;
-  public email!: string;
-  public password!: string;
-  public password_hash!: string;
-  public created_at!: string;
-  public updated_at!: string;
-
+  declare id: string;
+  declare name: string;
+  declare email: string;
+  declare password?: string | any;
+  declare password_hash: string;
+  declare admin?: boolean;
+  declare created_at: string;
+  declare updated_at: string;
   static initialize(sequelize: Sequelize) {
     super.init(
       {
@@ -43,7 +43,9 @@ class User extends Model<UserAtributes> {
         timestamps: false,
       }
     );
+
     this.addHook("beforeSave", async (user: User) => {
+      console.log(user.password, user.password_hash)
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 10);
       }
